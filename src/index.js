511b9d9de8ec122b1c1c0f48e4cf0f7d23481d4a
bwilson <bwilson@bridge2solutions.com>
2017-08-08 01:55:36 -0400

    converted to new material design components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import styles from 'styles';
-var componentHandler = require('exports-loader?componentHandler!material-design-lite/material.js')
+import * as mdc from 'material-components-web';
 var Template = require("./templates/template.handlebars");
-
 var app;
 
 function AppComponent() {
 	app = document.createElement('div');
 	app.id = "app";
+	app.className = "mdc-typography";
 	app.innerHTML = Template({});
+	document.body.className = "";
 	document.body.appendChild(app);
-	componentHandler.upgradeDom();
+
+    var drawerEl = document.querySelector('.mdc-persistent-drawer');
+    var MDCPersistentDrawer = mdc.drawer.MDCPersistentDrawer;
+    var drawer = new MDCPersistentDrawer(drawerEl);
+    document.querySelector('.demo-menu').addEventListener('click', function() {
+      drawer.open = !drawer.open;
+    });
+    drawerEl.addEventListener('MDCPersistentDrawer:open', function() {
+      console.log('Received MDCPersistentDrawer:open');
+    });
+    drawerEl.addEventListener('MDCPersistentDrawer:close', function() {
+      console.log('Received MDCPersistentDrawer:close');
+    });
 }
 
 AppComponent();
